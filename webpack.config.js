0b8fee63052cc4504091e026b13732e577c14890
lxy <yunxiaomxd@163.com>
2017-03-29 16:59:52 +0800

    first commit is webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname,'./dist/')
+	},
+	module: {
+	    rules: [{
+	      	test: /\.(js|jsx)$/,        //文件后缀、类型      
+	      	exclude: /node_modules/,    //排除这个目录的文件
+	      	loader: 'babel-loader',     //使用的加载器
+	      	options: {
+	        	presets: ['react', "es2015", 'stage-0'], //插件
+	    	}
+		}, {
+	        test: /\.css$/,
+	        use: ExtractTextPlugin.extract({
+	        	fallback: "style-loader",
+	          	use: "css-loader"
+	        })
+	    }, {
+　　　　　　test: /\.(png|jpg)$/,
+　　　　　　loader: 'url-loader?limit=8192&name=images/[name].[ext]'
+　　　  }]
+	},
+	plugins: [
+	    new ExtractTextPlugin("styles.css")
+	]
+}
