65a61fef56acd0b006bb87d249c06a232a997dd0
LonlyHeart <yunxiaomxd@163.com>
2017-04-04 15:07:47 +0800

    success is front-to-end

--- antdesign.js
+++ antdesign.js
@@ -1,54 +1,27 @@
-var http = require('http'),
-	url  = require("url"),
-    path = require("path"),
-    fs   = require("fs");
-
 var express = require("express");
 var app = new express();
 
-app.set('port', process.env.PORT || 3333); // 搭建服务器
+app.set('port', process.env.PORT || 3000); // 搭建服务器
 
-app.use(express.static('src')); // 托管静态文件
+app.use(express.static('dist')); // 托管静态文件
 app.use(require('body-parser')()); // body中间件解析post请求
 
 app.get('/', function(req,res){ // 访问根目录时重定向到首页
-
+    res.sendfile('index.html');
 });
 
-/*http.createServer(function(req,res){
-	var pathname=__dirname+url.parse(req.url).pathname;
-    if (path.extname(pathname)=="") {
-        pathname+="/src/index.html";
+app.post('/main', function(req,res){ // 访问首页
+    if(req.query.length !== 0){
+        if(req.query.username === "admin" && req.query.password === "123456"){
+            res.json({"success":true,"message":"登录成功..."});
+        }else{
+            res.json({"success":false,"message":"账号或密码错误..."});
+        }
+    }else{
+        res.json({"success":false,"message":"该系统不存在，请联系系统管理员..."});
     }
-    if (pathname.charAt(pathname.length-1)=="/"){
-        pathname+="index.html";
-    }
-    switch(path.extname(pathname)){
-        case ".html":
-            res.writeHead(200, {"Content-Type": "text/html"});
-            break;
-        case ".js":
-            res.writeHead(200, {"Content-Type": "text/javascript"});
-            break;
-        case ".css":
-            res.writeHead(200, {"Content-Type": "text/css"});
-            break;
-        case ".gif":
-            res.writeHead(200, {"Content-Type": "image/gif"});
-            break;
-        case ".jpg":
-            res.writeHead(200, {"Content-Type": "image/jpeg"});
-            break;
-        case ".png":
-            res.writeHead(200, {"Content-Type": "image/png"});
-            break;
-        default:
-            res.writeHead(200, {"Content-Type": "application/octet-stream"});
-    }
-    fs.readFile(pathname,function (err,data){
-    	console.log(data);
-        res.end(data);
-    });
-}).listen(3333);
+});
 
-console.log("server is start on port localhost:3333");*/
+app.listen(app.get('port'),function(){
+    console.log("server is start on port 3000");
+});
--- src/login/login.js
+++ src/login/login.js
@@ -4,64 +4,82 @@ import createBrowserHistory from 'history/createBrowserHistory';
 
 import 'antd/dist/antd';
 
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
-
-const path = require("path");
+import { Form, Icon, Input, Button, Checkbox, Spin, Modal } from 'antd';
 
 const FormItem = Form.Item;
 
 
 class Login extends React.Component {
-  constructor(props){
-    super(props);
-  }
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        // console.log('Received values of form: ', values);
-        // console.log(this.props.history.push("/main"));
-        // this.props.history.push("/main");
-        fetch(__dirname+'/main?'+JSON.stringfy(values)).then(function(res){
-          console.log(res.json());
-        }).then(function(data){
-          console.log(data);
-        }).catch(function(e){
-          console.err(e);
-        })
-      }
-    });
-  }
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <FormItem>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: '请输入账号!' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="请输入账号!" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: '请输入密码!' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="请输入密码!" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>记住密码</Checkbox>
-          )}
-          <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
-        </FormItem>
-      </Form>
-    );
-  }
+    constructor(props){
+        super(props);
+    }
+    state = {
+        loading: false
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let self = this;
+        self.props.form.validateFields((err, values) => {
+            if (!err) {
+                self.setState({loading: true});// 打开登录旋转
+                fetch(__dirname+'main?username='+values.userName+'&password='+values.password, {
+                    method: 'POST',
+                }).then(function(res){
+                    return res.json().then(function(data){ // 获取服务器返回的json对象
+                        return data;
+                    });
+                }).then(function(data){
+                    if(data.success === true){ // 如果验证用户信息正确，就跳转到主页面
+                        self.props.history.push("/main");
+                    }else{
+                        self.setState({loading: false});// 关闭登录旋转
+                        self.showModal(data.message);
+                    }
+                }).catch(function(e){
+                    console.error(e);
+                    self.setState({loading: false});// 关闭登录旋转
+                });
+            }
+        });
+    }
+    showModal = (message) => {
+        Modal.error({
+            title: '系统提示',
+            content: message
+        });
+    }
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Spin spinning={this.state.loading} tip="正在登录...">
+                <Form onSubmit={this.handleSubmit} className="login-form">
+                    <FormItem>
+                        {getFieldDecorator('userName', {
+                            rules: [{ required: true, message: '请输入账号!' }],
+                        })(
+                            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="请输入账号!" />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('password', {
+                            rules: [{ required: true, message: '请输入密码!' }],
+                        })(
+                            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="请输入密码!" />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('remember', {
+                            valuePropName: 'checked',
+                            initialValue: true,
+                        })(
+                            <Checkbox>记住密码</Checkbox>
+                        )}
+                        <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
+                    </FormItem>
+                </Form>
+            </Spin>
+        );
+    }
 }
 
 const LoginForm = Form.create()(Login);
